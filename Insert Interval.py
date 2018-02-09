5fe930739bb7b6b3f3f66a4322d8642fe1a33670
shank54 <shashanthota1@gmail.com>
2018-02-08 16:43:32 -0800

    Initial commit

---
+++ Insert Interval.py
@@ -0,0 +1,37 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x:x.start)        
+        if len(intervals)<=1:
+            return intervals
+        stk = []
+        stk.append(intervals[0].start)
+        stk.append(intervals[0].end)
+        for i in range(1,len(intervals)):
+            t = stk.pop()
+            if intervals[i].start<=t:
+                if t<=intervals[i].end:
+                    stk.append(intervals[i].end)
+                else:
+                    stk.append(t)
+            else:
+                stk.append(t)
+                stk.append(intervals[i].start)
+                stk.append(intervals[i].end)
+        newlst = []
+        while len(stk)>0:
+            a = stk.pop(0)
+            b = stk.pop(0)
+            newlst.append([a,b])
+        return newlst
